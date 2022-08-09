e956b2d4b0efe7badf651e1efee70296f9e81d87
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2022-08-08 22:04:14 -0300

    adicionando os métodos de acesso as rotas do servidor

--- src/controllers/AdsController.js
+++ src/controllers/AdsController.js
@@ -1,3 +1,19 @@
 module.exports = {
 
+    getCategories: (req, res) => {
+
+    },
+    addAction: (req, res) => {
+
+    },
+    getList: (req, res) => {
+
+    },
+    getItem: (req, res) => {
+
+    },
+    editAction: (req, res) => {
+
+    },
+
 };
